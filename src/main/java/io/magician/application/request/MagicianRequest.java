be176012b6b95d863362fe84e3076300538bbb99
Beerus <yuyemail@163.com>
2022-09-03 17:36:46 +0800

    Add a business thread pool, separate business processing from worker threads, and change the Chinese characters in the code to English

--- src/main/java/io/magician/application/request/MagicianRequest.java
+++ src/main/java/io/magician/application/request/MagicianRequest.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Extension of MartianHttpExchange
@@ -40,6 +41,7 @@ public class MagicianRequest {
     public void setHttpExchange(HttpExchange httpExchange, MagicianResponse magicianResponse) {
         this.httpExchange = httpExchange;
         this.magicianResponse = magicianResponse;
+        this.files = new ConcurrentHashMap<>();
 
         Map<String, ParamModel> paramMap = httpExchange.getParam();
         if (paramMap != null) {
